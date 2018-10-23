ff36a1f36d057f93998a15465f1465ab25efa723
Michael Fladischer <FladischerMichael@fladi.at>
2018-10-23 19:08:00 +0200

    Do not encode _ in systemd unit names.

--- _modules/linux_systemd.py
+++ _modules/linux_systemd.py
@@ -1,4 +1,4 @@
 import re
 
 def escape_path(path):
-    return re.sub('[^A-Za-z0-9\/]', '\\x2d', path.strip('/')).replace('/', '-')
+    return re.sub('[^A-Za-z0-9_\/]', '\\x2d', path.strip('/')).replace('/', '-')
